42b19355d3c87412db8e2331503c1e3108c2d0fc
Maik <maik@bybackfish.de>
2024-10-20 19:17:07 +0200

    add a argument for type to /git moreluck

--- src/main/kotlin/de/bybackfish/telosaddons/command/subcommands/MoreLuckCommand.kt
+++ src/main/kotlin/de/bybackfish/telosaddons/command/subcommands/MoreLuckCommand.kt
@@ -1,5 +1,6 @@
 package de.bybackfish.telosaddons.command.subcommands
 
+import com.mojang.brigadier.arguments.IntegerArgumentType
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import de.bybackfish.telosaddons.TelosAddons
 import de.bybackfish.telosaddons.core.commands.SubCommand
@@ -7,6 +8,7 @@ import de.bybackfish.telosaddons.core.feature.struct.FeatureState
 import de.bybackfish.telosaddons.features.BagFakeFeature
 import de.bybackfish.telosaddons.screen.OverlayMoveScreen
 import gg.essential.universal.UChat
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
 
 class MoreLuckCommand: SubCommand("moreluck", "Gives you more luck", "/telos moreluck") {
@@ -16,12 +18,19 @@ class MoreLuckCommand: SubCommand("moreluck", "Gives you more luck", "/telos mor
 
     override fun register(): LiteralArgumentBuilder<FabricClientCommandSource> {
         return LiteralArgumentBuilder.literal<FabricClientCommandSource?>("moreluck").executes {
-            val nextState = if(fakeBagFeature.state == FeatureState.ENABLED) FeatureState.DISABLED else FeatureState.ENABLED
-            UChat.chat("More luck is now ${nextState.name}")
-            fakeBagFeature.state = nextState
-            fakeBagFeature.fakeType = 1
-            fakeBagFeature.markDirty()
+            toggleFeature(0)
             1
-        }
+        }.then(ClientCommandManager.argument("type", IntegerArgumentType.integer(0, 6)).executes {
+            toggleFeature(IntegerArgumentType.getInteger(it, "type"))
+            1
+        })
+    }
+
+    fun toggleFeature(type: Int) {
+        val nextState = if(fakeBagFeature.state == FeatureState.ENABLED) FeatureState.DISABLED else FeatureState.ENABLED
+        UChat.chat("More luck is now ${nextState.name}")
+        fakeBagFeature.state = nextState
+        fakeBagFeature.fakeType = type
+        fakeBagFeature.markDirty()
     }
 }
